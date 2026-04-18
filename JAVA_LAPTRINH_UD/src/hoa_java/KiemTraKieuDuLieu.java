87e68df2f9772d24cd35db3f67833ccf65e6944e
Huynh Bich Tran <huynhbichtran01234@gmail.com>
2026-04-18 10:48:09 +0700

    Thêm file KiemTraKieuDuLieu

---
+++ JAVA_LAPTRINH_UD/src/hoa_java/KiemTraKieuDuLieu.java
@@ -0,0 +1,17 @@
+package hoa_java;
+
+import java.util.Scanner;
+
+public class KiemTraKieuDuLieu { 
+
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.println("Nhập số nguyên:");
+        int a = sc.nextInt();
+        
+   
+        
+    }
+}
