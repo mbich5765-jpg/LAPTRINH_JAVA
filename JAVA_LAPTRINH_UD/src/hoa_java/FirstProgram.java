bd1856f2b9bce9655016667495f530548aec014f
Huynh Bich Tran <huynhbichtran01234@gmail.com>
2026-04-18 10:05:19 +0700

    Bài tập eclipse buổi 2 của Java

---
+++ JAVA_LAPTRINH_UD/src/hoa_java/FirstProgram.java
@@ -0,0 +1,10 @@
+package hoa_JAVA;
+
+public class FirstProgram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Xin chào các Thím!");
+	}
+
+}
---
+++ JAVA_LAPTRINH_UD/src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module JAVA_LAPTRINH_UD {
+}
